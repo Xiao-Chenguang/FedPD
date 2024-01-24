1511d262739c10bdda5648c44f64405073a8a0f2
Xiao-Chenguang <973990275@qq.com>
2024-01-24 22:16:32 +0000

    pd out most comments

--- core/imbalance_fl.py
+++ core/imbalance_fl.py
@@ -15,13 +15,17 @@ class ImbalanceFL(PrimalDualFedAlgorithm):
         lambda_var = torch.zeros(self.config.n_workers)
         return ImFL_server_state(global_round=0, model=model, lambda_var=lambda_var)
 
+    ### global pd step
     def step(self):
         sss = self.server_state
         weights = (1. + sss.lambda_var - torch.mean(sss.lambda_var))
         client_losses = torch.tensor(self.primal_fed_algorithm.clients_evaluate())
+        ### clients synchronize step with primal and dual updates
         self.primal_fed_algorithm.fit(weights, self.config.n_p_steps)
         model_new = self.primal_fed_algorithm.server_state.model
+        ### update lambda
         lambda_new = sss.lambda_var + self.config.lambda_lr * (client_losses - torch.mean(client_losses) - self.config.tolerance_epsilon) / self.config.n_workers
+        ### clip lambda
         lambda_new = torch.clamp(lambda_new, min=0., max=100.)
         self.server_state = ImFL_server_state(global_round=sss.global_round+1, model=model_new, lambda_var=lambda_new)
 
